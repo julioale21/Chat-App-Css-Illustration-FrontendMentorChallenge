a1a73714c94831761ba99c4a51f548d9da8cfbd3
Julio Romero <julio_ale21@hotmail.com>
2021-06-30 21:40:34 -0300

    chore: added and configured eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
+import React from "react";
 
 function App() {
-
   return (
     <div>
       <h1>chat app</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
