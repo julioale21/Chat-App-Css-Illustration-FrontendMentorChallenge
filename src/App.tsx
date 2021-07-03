32d4119c984f8d0a7519dbd63e56fec2233b34c3
Julio Romero <julio_ale21@hotmail.com>
2021-07-03 13:53:56 -0300

    chore: added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
 import React from "react";
-import { Stack } from "@chakra-ui/react";
+import { Stack, Text, Link } from "@chakra-ui/react";
 import BackgroundImageLeft from "./components/BackgroundImageLeft";
 import PhoneSection from "./components/sections/PhoneSection";
 import DescriptionSection from "./components/sections/DescriptionSection";
+import BackgroundImageRight from "./components/BackgroundImageRight";
 
 function App() {
   return (
     <Stack
-      backgroundColor="lightGradishViolet"
-      height="100%"
-      minHeight="100vh"
+      backgroundColor="white"
+      overflow="hidden"
       padding={6}
+      position="relative"
       textAlign="center"
       width="100%"
     >
       <BackgroundImageLeft />
-      <Stack direction={{ base: "column", md: "row" }} zIndex={1}>
-        <PhoneSection />
-        <DescriptionSection />
+      <BackgroundImageRight />
+      <Stack direction="column">
+        <Stack direction={{ base: "column", md: "row" }} zIndex={1}>
+          <PhoneSection />
+          <DescriptionSection />
+        </Stack>
+        <Stack paddingTop={{ base: "50px", lg: "0px" }} spacing={0} zIndex={1}>
+          <Text fontSize="0.7rem" fontWeight="bold">
+            Frontend Mentor Challenge.
+          </Text>
+          <Text fontSize="0.6rem">
+            Solved by{" "}
+            <Link isExternal href="https://github.com/julioale21">
+              @julioale21
+            </Link>
+          </Text>
+        </Stack>
       </Stack>
     </Stack>
   );
